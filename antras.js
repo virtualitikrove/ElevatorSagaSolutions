2dee9ab957040490adfd1800b9a567e7b62d3c87
Deividas <deividasvaliunas@panko.lt>
2018-09-06 21:38:28 +0300

    Antras lygis - done

---
+++ antras.js
@@ -0,0 +1,18 @@
+{
+    init: function(elevators, floors) {
+        var elevator = elevators[0]; // Let's use the first elevator
+
+        // Whenever the elevator is idle (has no more queued destinations) ...
+        elevator.on("idle", function() {
+            // let's go to all the floors (or did we forget one?)
+            elevator.goToFloor(0);
+            elevator.goToFloor(1);
+            elevator.goToFloor(2);
+            elevator.goToFloor(3);
+            elevator.goToFloor(4);
+        });
+    },
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
+}
