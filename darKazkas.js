ac89ae8873da40f35a870fa9881f1f075220720c
Deividas <deividasvaliunas@panko.lt>
2018-09-11 23:00:52 +0300

    Dar karta daryta ketvirta uzduotis

---
+++ darKazkas.js
@@ -0,0 +1,43 @@
+{
+
+    init: function(elevators, floors) {
+        var elevator = elevators[0]; // Let's use the first elevator
+
+        elevators.forEach(function (elevator) {
+
+
+
+            elevator.on("passing_floor", function(floorNum, direction) {
+                if (direction == "up" && floor.arAukstyn) {  elevator.goToFloor(floorNum, true);    } //paimam pakeleivius
+                if (direction == "down" && floor.arZemyn) {  elevator.goToFloor(floorNum, true);    } //paimam pakeleivius
+
+
+            }); // passing floor pabaiga
+             elevator.on("floor_button_pressed", function(floor) {
+             elevator.goToFloor(floor);
+              });
+
+
+        }); //elevatoriu funkcijos
+
+        floors.forEach(function (floor) {
+            floor.on("up_button_pressed", function() {
+                floor.arAukstyn = true; // suzinm, ar yra iskvietimas aukstyn
+                
+            });
+
+            floor.on("down_button_pressed", function() {
+                floor.arZemyn = true; // suzinm, ar yra iskvietimas zemyn
+
+            });
+
+
+
+
+
+        }); //floors pabaiga
+    },
+        update: function(dt, elevators, floors) {
+            // We normally don't need to do anything here
+        }
+}
